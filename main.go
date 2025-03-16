e908fcc235172589dcb6c12eab46668f7389b570
Tevin Tchuinkam <tevintchuinkam@gmail.com>
2025-03-16 23:06:41 +0100

    scrape event data

--- main.go
+++ main.go
@@ -2,18 +2,35 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"mainz-events/internal/calendar"
 	"mainz-events/internal/scraper"
 )
 
 func main() {
 	iterator := scraper.NewEventLinkIterator()
+	cal := calendar.New()
+
+	defer func() {
+		if r := recover(); r != nil {
+			log.Fatalln(r)
+		}
+		cal.Save()
+	}()
 
 	for {
 		link, hasMore := iterator.Next()
 		if !hasMore {
 			break
 		}
-		fmt.Println(link)
+		event, err := scraper.ScrapeEvent(link)
+		if err != nil {
+			log.Fatalln(err)
+			continue
+		}
+		if event != nil && event.Title != "" {
+			fmt.Println("added event", event)
+			cal.AddEvent(*event)
+		}
 	}
-
 }
